e4fb024f0960bd96bcd837caf9a5528cdac5c619
MURLIDHAR DNYANESHWAR MANDKE <79237912+murlidharmandke@users.noreply.github.com>
2022-05-20 15:58:15 +0530

    E-COMM WEB
    Better Understanding of api and e store

---
+++ Shop/admin/src/components/topbar/Topbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./topbar.css";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Mpify Admin Page </span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img src="https://www.kindpng.com/picc/m/475-4750705_school-administrator-icon-png-transparent-png.png" alt="" className="topAvatar" />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ Shop/admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA6VsNhZUkzvh1dvMmGUXy_NY-5DLMqJV8",
+  authDomain: "shop-d3077.firebaseapp.com",
+  projectId: "shop-d3077",
+  storageBucket: "shop-d3077.appspot.com",
+  messagingSenderId: "202549488726",
+  appId: "1:202549488726:web:d35858d9bf497781415ff7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
